dc17e5e87d0a0bca3883f8d2ce0053df4c2dc9a2
Ray Callegari <14826@holbertonstudents.com>
2026-08-18 10:19:09 +0000

    submitting recursive code for task 3, in style and doc.

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,24 @@
+#include "binary_trees.h"
+
+/**
+* binary_tree_delete - using recursion to traverse
+* the tree focusing on the left link.
+* @tree: a pointer to the root node where deletion
+* is being made.
+* Return: void - the fail state is nothing.
+*/
+
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree != NULL)
+	{
+		if (tree->left != NULL)
+			binary_tree_delete(tree->left);
+
+		if (tree->right != NULL)
+			binary_tree_delete(tree->right);
+
+		if (tree->left == NULL && tree->right == NULL)
+			free(tree);
+	}
+}
